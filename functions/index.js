6ac17ced52f022af23a901982cd6ab3cbe768fd6
Milindu Sanoj Kumarage <agentmilindu@gmail.com>
2019-02-02 13:04:43 +0530

    Add ability to serve installer scripts on request

--- functions/index.js
+++ functions/index.js
@@ -2,17 +2,26 @@
 
 const functions = require('firebase-functions');
 const express = require('express');
-const app = express();
+const {Storage} = require('@google-cloud/storage');
 
-const cors = require('cors')({origin: true});
-app.use(cors);
+
+const app = express();
+const storage = new Storage();
+const bucket = storage.bucket('installer-to.appspot.com');
 
 app.get('/', (req, res) => {
   res.json({ status: 'OK' });
 });
 
 app.get('/:package', (req, res) => {
-  res.json( req.params );
+    try {
+        const file = bucket.file(req.params.package + '/installer.sh');
+        res.set('Content-Type', 'text/plain');
+        const readStream = file.createReadStream();
+        readStream.pipe(res);
+    } catch (e) {
+        res.send(e);
+    }
 });
 
 exports.app = functions.https.onRequest(app);
