e87e1b99c88a52e339a69de09f3c805130520c37
Milindu Sanoj Kumarage <agentmilindu@gmail.com>
2019-02-03 12:07:10 +0530

    [#1] Add support for more package managers

--- installers/git/installer.sh
+++ installers/git/installer.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-YUM_CMD=$(which yum)
-APT_GET_CMD=$(which apt-get)
-DNF_CMD=$(which dnf)
+YUM_CMD=$(which yum) # yum package manager for RHEL & CentOS
+DNF_CMD=$(which dnf) # dnf package manager for new RHEL & CentOS
+APT_GET_CMD=$(which apt-get) # apt package manager for Ubuntu & other Debian based distributions
+PACMAN_CMD=$(which pacman) # pacman package manager for ArchLinux
+APK_CMD=$(which apk) # apk package manager for Alpine
 
  if [ ! -z $APT_GET_CMD ]; then
     sudo apt-get update
@@ -11,8 +13,12 @@ DNF_CMD=$(which dnf)
     sudo dnf install git
  elif [ ! -z $YUM_CMD ]; then
     sudo yum install git
+ elif [ ! -z $PACMAN_CMD ]; then
+    pacman -Sy git
+ elif [ ! -z $APK_CMD ]; then
+    sudo apk add git
  else
-    echo "error can't install package"
+    echo "Couldn't install package"
     exit 1;
  fi
 
