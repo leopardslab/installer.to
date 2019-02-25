0b62667145e5fb9f02ffdf2f32828eae88f2cf68
VibhorCodecianGupta <guptavibhu98@gmail.com>
2019-02-25 06:07:48 +0530

    nginx installation script

---
+++ installers/nginx/installer.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+YUM_CMD=$(which yum) # yum package manager for RHEL & CentOS
+DNF_CMD=$(which dnf) # dnf package manager for new RHEL & CentOS
+APT_GET_CMD=$(which apt-get) # apt package manager for Ubuntu & other Debian based distributions
+PACMAN_CMD=$(which pacman) # pacman package manager for ArchLinux
+APK_CMD=$(which apk) # apk package manager for Alpine
+
+ if [ ! -z $APT_GET_CMD ]; then
+    sudo apt-get update
+    sudo apt-get install nginx
+ elif [ ! -z $DNF_CMD ]; then
+    sudo dnf install nginx
+ elif [ ! -z $YUM_CMD ]; then
+    sudo yum install nginx
+ elif [ ! -z $PACMAN_CMD ]; then
+    sudo pacman -S nginx
+ elif [ 1 -z $APK_CMD ]; then
+    sudo apk update
+    sudo apk add nginx
+ else
+    echo "Couldn't install package"
+    exit 1;
+ fi
+
+nginx -v
