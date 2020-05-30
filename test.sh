83cf0274ab2c8dad65abd202953e55b7d62261ca
Heshdude <heshan.geasman@gmail.com>
2020-05-30 21:44:04 +0530

    Run shellspec on test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,13 @@
-for word in "$@"
- do echo "$word" 
+#!/bin/bash
+
+#declare -a X
+X=()
+
+for word in "$@"; do
+#  echo $(dirname "$word")
+  X+=$(dirname "$word")" "
 done
+
+CHANGED=$(echo $X | tr ' ' '\n' | sort | uniq | xargs)
+
+shellspec $CHANGED
