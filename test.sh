69edd28b58e47eba0155dbfb93d5151661ba72f1
Heshdude <heshan.geasman@gmail.com>
2020-05-30 22:11:44 +0530

    Cuting our only the top two dirs from direpath

--- test.sh
+++ test.sh
@@ -5,10 +5,10 @@ X=()
 
 for word in "$@"; do
 #  echo $(dirname "$word")
-  X+=$(dirname "$word")" "
+  X+=$(dirname "$word " | cut -d "/" -f "1 2")" "
 done
 
 CHANGED=$(echo $X | tr ' ' '\n' | sort | uniq | xargs)
 
 echo "Running tests on $CHANGED"
-shellspec $CHANGED
+#shellspec $CHANGED
